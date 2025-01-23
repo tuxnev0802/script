5383e9b9b6b6f5ce80131a4575aa33db496da66f
tuxnev0802 <huyhuynh123k@gmail.com>
2025-01-23 21:42:00 +0700

    Add files via upload

--- fix-lag.lua
+++ fix-lag.lua
@@ -36,5 +36,8 @@ for i, v in pairs(game:GetDescendants()) do
         v.Reflectance = 0
     elseif v:IsA("Clothing") then
         v:Destroy()
+    elseif v:IsA("Highlight") then
+        v.OutlineTransparency = 1
+        v.FillTransparency = 1
     end
 end
